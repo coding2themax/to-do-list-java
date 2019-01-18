65397cd267a36f338c9a790d3d1c805f5ffd2ab1
Max <maximilian.quinones@gmail.com>
2019-01-17 22:55:33 -0700

    init

---
+++ src/main/java/com/codingtothemax/kata/todolistjava/TodoListJavaApplication.java
@@ -0,0 +1,14 @@
+package com.codingtothemax.kata.todolistjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListJavaApplication.class, args);
+	}
+
+}
+
